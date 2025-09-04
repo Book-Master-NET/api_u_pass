f3fac1706d75d46e0657301475ae7b5b2125b7f1
REMR11 <ronaldmejia0211@gmail.com>
2025-09-04 16:51:18 -0600

    feat: proyecto base

---
+++ src/test/java/com/fqstudio/attendance_system/UPassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fqstudio.attendance_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UPassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
